36189d4fd01e63abf4448ebc3dd5426b48a3bdff
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2025-11-14 23:43:44 -0700

    Deleted build folder and refactored sorter

--- typescript/sort/src/index.ts
+++ typescript/sort/src/index.ts
@@ -1,31 +1,7 @@
-class Sorter {
-    public collection: number[] | string;
+import { Sorter } from './sorter.js';
+import { NumberCollection } from './numberCollection.js';
 
-    constructor (collection: number[] | string) {
-        this.collection = collection;
-    }
-
-    sort (): void {
-        const { length } = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length-i-1; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j]! > this.collection[j+1]!) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j+1]!;
-                        this.collection[j+1] = leftHand!;
-                    }
-                }
-
-                if (typeof this.collection === 'string') {
-                    
-                }
-
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([1, 9, 8, 7, 5]);
+const numberCollection = new NumberCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numberCollection);
 sorter.sort();
+console.log(numberCollection.data);
---
+++ typescript/sort/src/numberCollection.ts
@@ -0,0 +1,21 @@
+export class NumberCollection {
+    public data: number[];
+
+    constructor (data: number[]) {
+        this.data = data;
+    }
+
+    get length(): number {
+        return this.data.length;
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        return this.data[leftIndex]! > this.data[rightIndex]!;
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        const temp = this.data[leftIndex];
+        this.data[leftIndex] = this.data[rightIndex]!;
+        this.data[rightIndex] = temp!;
+    }
+}
